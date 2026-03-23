aa4dd55ae0fe0b9ae38988f28371b0d1f7edccbf
talesCPV <tales@flexibus.com.br>
2026-03-23 10:35:40 -0300

    by_script

---
+++ commit.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Upload files to Github - git@github.com:talesCPV/tower.git
+
+read -p "Are you sure to commit Tower to GitHub ? (Y/n)" -n 1 -r
+echo 
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+
+    git init
+
+    git add assets/
+    git add script.js
+    git add style.css
+    git add index.html
+    git add commit.sh
+    
+    git commit -m "by_script"
+
+    git branch -M main
+    git remote add origin git@github.com:talesCPV/tower.git
+    git remote set-url origin git@github.com:talesCPV/tower.git
+
+    git push -u -f origin main
+
+fi
