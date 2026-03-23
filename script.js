6e1ecc19bc819e075e8f952c9575dd95dbe6af65
talesCPV <tales@flexibus.com.br>
2026-03-23 16:56:42 -0300

    by_script

--- script.js
+++ script.js
@@ -15,13 +15,44 @@ const game = new Object
 
 
 function drawGrid(){
-    document.querySelector('.grid').style.display = game.grid ? 'block' : 'none'
+    const grid =  document.querySelector('#grid')
+    grid.innerHTML = ''
+    for(let y=0; y<10; y++){
+        const line = document.createElement('div')
+        line.className = 'line-path'
+        grid.appendChild(line)
+        for(let x=0; x<13; x++){
+            const cel = document.createElement('div')
+            cel.className = 'cel-path'
+            cel.id = `cel-${y}-${x}`
+            line.appendChild(cel)
+        }
+    }
+
 }
-
-
 
 document.querySelector('#btn-grid').addEventListener('click',()=>{
     game.grid = !game.grid
-    drawGrid()
-    document.querySelector('#btn-grid').innerHTML = game.grid ? 'NO GRID' : 'GRID'
+    if(game.grid){
+        document.querySelector('#grid').classList.add('grid')
+        document.querySelector('#btn-grid').innerHTML = 'NO GRID'
+    }else{
+        document.querySelector('#grid').classList.remove('grid')
+        document.querySelector('#btn-grid').innerHTML = 'GRID'
+    }
 })
+
+/*
+document.querySelector('#grid').addEventListener('mousemove',(e)=>{
+    const bounds = e.target.getBoundingClientRect();
+    const x = Math.floor((e.x - Math.floor(bounds.left) -15)/25.6)
+    const y = Math.floor((e.y - Math.floor(bounds.top)  -15)/25.6)
+    if(x>=0 && x<13 && y>=0 && y<10){
+        console.log(x,y)
+
+    }
+})
+*/
+
+
+drawGrid()
